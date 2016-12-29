eab6734f4626ab85aa7299426b095c0c6b57ec99
Sam Pagenkopf <ssaammp@gmail.com>
2016-12-29 17:04:22 -0600

    tile scrolling and memory viewer

--- game.lua
+++ game.lua
@@ -5,30 +5,45 @@ local draw = require "draw"
 -- tile_off says where to start, moves up with the game
 -- for each tile, true = sand, false = none
 local tiles = {}
-local tile_off = 4
+local tile_off = 0
+local scroll = 0
 
 return {
    init = function ()
       for y = 0,15 do
 	 tiles[y] = {}
 	 for x = 0,15 do
-	    tiles[y][x] = true;
+	    tiles[y][x] = true
 	 end
       end
    end,
 
    update = function ()
+      scroll = scroll + 1
+      tile_off = math.floor(scroll / 16)
+      local maxoff = tile_off+15
+      if not tiles[maxoff] then
+	 tiles[tile_off-1] = nil
+	 tiles[maxoff] = {}
+	 for x = 0,15 do
+	    tiles[maxoff][x] = true
+	 end
+      end
+
+      local rx = math.floor(math.random() * 16)
+      local ry = math.floor(math.random() * 16) + tile_off
+      tiles[ry][rx] = false
    end,
 
    draw = function ()
       -- Draw 11 rows of tiles; max possible visible
-      for y = 0,11 do
+      for y = tile_off,tile_off+11 do
       	 for x = 0,15 do
-       	    local tilex = x % 2 + (tiles[y+tile_off][x] and 0 or 2);
+       	    local tilex = x % 2 + (tiles[y+4][x] and 0 or 2)
        	    local tiley = y % 2-- TODO: depth randomization
        	    draw.add(tilex, tiley, x*16, y*16)
        	 end
       end
-      draw.draw(0, 0)
+      draw.draw(0, -scroll)
    end,
 }
--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ love.run = function ()
    love.math.setRandomSeed(os.time())
    state.push(game)
 
+   love.graphics.setDefaultFilter("nearest", "nearest")
    canvas = love.graphics.newCanvas(240, 160)
 
    while true do
@@ -18,11 +19,12 @@ love.run = function ()
 	 love.handlers[name](a,b,c,d,e,f)
       end
 
-      love.graphics.setBlendMode("alpha", "alphamultiply")
       state.update()
       canvas:renderTo(state.draw)
       love.graphics.setBlendMode("replace", "premultiplied")
-      love.graphics.draw(canvas)
+      love.graphics.draw(canvas, 0, 0, 0, 3, 3)
+      love.graphics.setBlendMode("alpha", "alphamultiply")
+      love.graphics.print(collectgarbage("count"))
       love.graphics.present()
    end
 end
