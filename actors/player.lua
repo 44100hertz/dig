f1db02590fc88ffc8270f4afd1b408f4ab2efe73
Sam Pagenkopf <ssaammp@gmail.com>
2016-12-29 20:48:11 -0600

    player states

--- actors/player.lua
+++ actors/player.lua
@@ -3,29 +3,64 @@ local tiles = require "tiles"
 
 local origin_x, origin_y = 8, 16
 
+local bool2num = function (bool) return bool and 1 or 0 end
+
+local loadstate = function (self, state)
+   self.timer = 0
+   self.state = state
+end
+
+local jump, floor, air
+
+move = function (self)
+   self.dx =
+      bool2num(love.keyboard.isScancodeDown("right")) -
+      bool2num(love.keyboard.isScancodeDown("left"))
+end
+
+jump = function (self)
+   if self.timer == 3 then
+      move(self)
+   elseif self.timer == 4 then
+      self.dy = -2
+      loadstate(self, air)
+   end
+end
+
+floor = function (self)
+   local tx, ty = tiles.collide(self.x, self.y)
+   if not tx then
+      loadstate(self, air)
+      return
+   end
+   move(self)
+   if love.keyboard.isScancodeDown("x") then
+      tiles.destroy(tx, ty)
+   elseif love.keyboard.isScancodeDown("z") then
+      loadstate(self, jump)
+   end
+end
+
+air = function (self)
+   local tx, ty = tiles.collide(self.x, self.y)
+   if tx then
+      self.state = floor
+      self.dy = 0
+      self.y = math.floor(self.y / 16) * 16
+   else
+      self.dy = math.min(self.dy + 1/8, 2)
+   end
+end
+
 return {
    init = function (self)
       self.dx, self.dy = 0,0
+      loadstate(self, air)
    end,
 
    update = function (self)
-      local tx, ty = tiles.collide(self.x, self.y)
-      if tx then
-	 self.dy = 0
-	 self.y = math.floor(self.y / 16) * 16
-	 if love.keyboard.isScancodeDown("x") then
-	    tiles.destroy(tx, ty)
-	 elseif love.keyboard.isScancodeDown("z") then
-	    self.dy = -2
-	 end
-      else
-	 self.dy = math.min(self.dy + 1/8, 2)
-      end
-
-      local bool2num = function (bool) return bool and 1 or 0 end
-      self.dx =
-	 bool2num(love.keyboard.isScancodeDown("right")) -
-	 bool2num(love.keyboard.isScancodeDown("left"))
+      self.timer = self.timer + 1
+      self:state()
    end,
 
    draw = function (self)
