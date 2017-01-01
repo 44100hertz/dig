ba0a2757840a827817e7ac27c06898e181d275af
44100hertz <ssaammp@gmail.com>
2017-01-01 15:56:33 -0600

    more tabs

--- src/draw.lua
+++ src/draw.lua
@@ -11,13 +11,13 @@ return {
    add = function (x, y, draw_x, draw_y, w, h, flip)
       if not quads[y] then quads[y] = {} end
       if not quads[y][x] then
-	 w = w or 1
-	 h = h or 1
-	 quads[y][x] = love.graphics.newQuad(
-	    x * 16, y * 16,
-	    w * 16, h * 16,
-	    assets_width, assets_height
-	 )
+         w = w or 1
+         h = h or 1
+         quads[y][x] = love.graphics.newQuad(
+            x * 16, y * 16,
+            w * 16, h * 16,
+            assets_width, assets_height
+         )
       end
       draw_x, draw_y = math.floor(draw_x), math.floor(draw_y)
       sb:add(quads[y][x], draw_x, draw_y, 0, flip and -1 or 1, 1)
