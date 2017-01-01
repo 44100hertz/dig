342e071878101b2b7d2b71cf05e6b7cefc58827f
44100hertz <ssaammp@gmail.com>
2017-01-01 13:20:16 -0600

    tabs

--- src/actors.lua
+++ src/actors.lua
@@ -7,25 +7,25 @@ return {
 
    update = function (scroll)
       for k,v in ipairs(actors) do
-	 v.class.update(v, scroll)
-	 if v.dx then v.x = v.x + v.dx end
-	 if v.dy then v.y = v.y + v.dy end
-	 v.x = v.x % 240
-	 if v.timer then v.timer = v.timer + 1 end
+         v.class.update(v, scroll)
+         if v.dx then v.x = v.x + v.dx end
+         if v.dy then v.y = v.y + v.dy end
+         v.x = v.x % 240
+         if v.timer then v.timer = v.timer + 1 end
       end
    end,
 
    draw = function ()
       for k,v in ipairs(actors) do
-	 if v.class.draw then v.class.draw(v) end
+         if v.class.draw then v.class.draw(v) end
       end
       for k,v in ipairs(actors) do
-	 if v.die then table.remove(actors, k) end
+         if v.die then table.remove(actors, k) end
       end
    end,
 
    add = function (actor)
       actor.class.init(actor)
       table.insert(actors, actor)
-   end
+   end,
 }
