adb5cd8d0c4fd9736fc10a0a85c8db85b85cad38
44100hertz <ssaammp@gmail.com>
2017-01-02 16:29:18 -0600

    auto drawing

--- src/actors.lua
+++ src/actors.lua
@@ -1,3 +1,5 @@
+local draw = require "draw"
+
 local actors
 
 return {
@@ -6,18 +8,26 @@ return {
    end,
 
    update = function (scroll)
-      for k,v in ipairs(actors) do
-         v.class.update(v, scroll)
-         if v.dx then v.x = v.x + v.dx end
-         if v.dy then v.y = v.y + v.dy end
-         v.x = v.x % 240
+      for _,v in ipairs(actors) do
+         if v.class.update then
+            v.class.update(v, scroll)
+         end
+         if v.x then
+            if v.dx then v.x = v.x + v.dx end
+            if v.dy then v.y = v.y + v.dy end
+         end
          if v.timer then v.timer = v.timer + 1 end
       end
    end,
 
    draw = function ()
-      for k,v in ipairs(actors) do
+      for _,v in ipairs(actors) do
          if v.class.draw then v.class.draw(v) end
+         if v.fx then
+            local ox = v.ox or 0
+            local oy = v.oy or 0
+            draw.add(v.fx, v.fy, v.x-ox, v.y-oy, v.sx, v.sy, v.flip)
+         end
       end
       for k,v in ipairs(actors) do
          if v.die then table.remove(actors, k) end
--- src/actors/gem.lua
+++ src/actors/gem.lua
@@ -2,14 +2,8 @@ local draw = require "draw"
 
 return {
    init = function (self)
-      self.kind = self.kind or math.ceil(math.random() * 4)
-      self.big = self.big or math.ceil(math.random() * 2)
-   end,
-
-   update = function () end,
-   
-   draw = function (self)
-      -- type must be 4, 5, 6
-      draw.add(self.kind+3, self.big+1, self.x, self.y)
+      self.sx, self.sy = 1,1
+      self.fx = self.kind or math.ceil(math.random() * 4) + 3
+      self.fy = self.big or math.ceil(math.random() * 2) + 1
    end
 }
