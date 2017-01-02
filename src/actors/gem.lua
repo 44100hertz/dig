5902fa2c205fcec0c2f76a5ecab47342075af2fe
44100hertz <ssaammp@gmail.com>
2017-01-02 16:31:26 -0600

    gem graphics fix

--- src/actors/gem.lua
+++ src/actors/gem.lua
@@ -3,7 +3,7 @@ local draw = require "draw"
 return {
    init = function (self)
       self.sx, self.sy = 1,1
-      self.fx = self.kind or math.ceil(math.random() * 4) + 3
+      self.fx = self.kind or math.ceil(math.random() * 4) + 2
       self.fy = self.big or math.ceil(math.random() * 2) + 1
    end
 }
