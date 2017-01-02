6835553c6f1eee03fdb420c293c6441c08f627d4
44100hertz <ssaammp@gmail.com>
2017-01-02 16:28:58 -0600

    local canvas

--- main.lua
+++ main.lua
@@ -8,17 +8,17 @@ love.run = function ()
    state.push(game)
 
    love.graphics.setDefaultFilter("nearest", "nearest")
-   canvas = love.graphics.newCanvas(240, 160)
+   local canvas = love.graphics.newCanvas(240, 160)
 
    while true do
       love.event.pump()
       for name, a,b,c,d,e,f in love.event.poll() do
-	 if name == "quit" then
-	    if not love.quit or not love.quit() then
-	       return a
-	    end
-	 end
-	 love.handlers[name](a,b,c,d,e,f)
+         if name == "quit" then
+            if not love.quit or not love.quit() then
+               return a
+            end
+         end
+         love.handlers[name](a,b,c,d,e,f)
       end
 
       love.graphics.setBlendMode("alpha", "alphamultiply")
