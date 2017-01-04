2f55b40db8883d94b034a17bbdebe37542dca8e3
Sam Pagenkopf <pagenkopfsd@msoe.edu>
2017-01-04 03:07:21 -0600

    removed silly particle code

--- src/actors/particle.lua
+++ src/actors/particle.lua
@@ -6,7 +6,6 @@ return {
    end,
 
    draw = function (self)
-      local frame = math.floor(self.timer / 5) + 7
-      draw.add(frame, self.sprite, self.x, self.y, 1, 1, self.flip)
+      self.fx = math.floor(self.timer / 5) + 7
    end,
 }
