47ecec2e56b8cd637b99ad8892b2a5c476affbdf
Sam Pagenkopf <ssaammp@gmail.com>
2022-11-03 21:50:39 -0500

    fix end screen for love2d 11.4

--- src/end.lua
+++ src/end.lua
@@ -17,8 +17,8 @@ return {
    end,
 
    draw = function ()
-      love.graphics.setBlendMode("multiply")
-      love.graphics.setColor(254, 254, 254)
+      love.graphics.setBlendMode("multiply", "premultiplied")
+      love.graphics.setColor(0.99, 0.99, 0.99)
       love.graphics.rectangle("fill",0,0,240,160)
    end,
 }
